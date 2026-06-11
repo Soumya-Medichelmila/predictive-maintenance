43f620642ec28519494d6cba75f13672a4ec3999
Soumya-Medichelmila <soumya.medichelmila@gmail.com>
2026-06-11 23:58:45 +0530

    Create database tables on startup

--- app/main.py
+++ app/main.py
@@ -2,16 +2,18 @@ from fastapi import FastAPI
 
 from app.routes.predict import router as predict_router
 from app.routes.history import router as history_router
-#from app.routes.explain import router as explain_router
+
+from app.database.database import Base, engine
+from app.database.models import Prediction
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Predictive Maintenance API"
 )
 
 app.include_router(predict_router)
-#app.include_router(explain_router)
 app.include_router(history_router)
-
 
 @app.get("/")
 def home():
